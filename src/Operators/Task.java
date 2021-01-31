d326166b1dd5f67f9c6a0dcac161daab2daa2560
ZitronCrazy <zitro5@protonmail.com>
2021-01-31 02:00:00 +0100

    Operators

---
+++ src/Operators/Task.java
@@ -0,0 +1,20 @@
+package Operators;
+
+public class Task {
+    public static void main(String[] args) {
+
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNotRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
+
+
+    }
+}
